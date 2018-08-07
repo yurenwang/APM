b951113244a2a3e4849fbfafbba5fb65fdadb92b
YWANG <ywang@s1inc.com>
2018-08-07 16:24:47 -0400

    Add custom Pipe

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,12 @@
+import { PipeTransform, Pipe } from "@angular/core";
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+export class ConvertToSpacesPipe implements PipeTransform {
+    
+    transform(value: string, charactor: string): string {
+        return value.replace(charactor, ' ');
+    }
+    
+}
