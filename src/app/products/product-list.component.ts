29bbbe073ddf0ce700734502c9fb94a44c8c6ae2
YWANG <ywang@s1inc.com>
2018-08-08 10:29:00 -0400

    Add nested star component and data input/output with it

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -60,6 +60,10 @@ export class ProductListComponent implements OnInit {
         this.listFilter = '';
     }
 
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
+
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
